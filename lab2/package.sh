e6a89a7d181e298910df46bdddacb2b253a00932
Tong Mu <dkwingsmt@gmail.com>
2015-11-01 17:20:04 -0600

    Delete some mis-added files.

--- lab2/package.sh
+++
@@ -1,7 +0,0 @@
-TGT_NAME=lab2release
-TGT_DIR=~/$TGT_NAME
-rm -rf $TGT_DIR
-mkdir $TGT_DIR
-git archive master | tar -x -C $TGT_DIR
-zip -r5 ${TGT_NAME}.zip $TGT_DIR
-
--- lab2/test_main.sh
+++
@@ -1,1 +0,0 @@
-make main && build/lab2 configs/partial_hamlet_act_ii_script.txt
