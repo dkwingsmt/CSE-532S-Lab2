3f0986e132853d2638cb62ac5f8722b0c1f9a752
Tong Mu <dkwingsmt@gmail.com>
2015-11-01 17:05:23 -0600

    Deleted old files.

--- _old_code/common.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-// From http://stackoverflow.com/questions/8518743/get-directory-from-file-path-c
-string dirnameOf(const string& fname) {
-    size_t pos = fname.find_last_of("\\/");
-    return (std::string::npos == pos)
-        ? ""
-        : fname.substr(0, pos+1);
-}
